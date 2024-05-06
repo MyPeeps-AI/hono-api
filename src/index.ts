22c73b565ddf48f7be510c0c97c2d035088191c0
Jens Krause <jens@rasta.online>
2024-05-06 18:58:09 +0700

    Add KeyValue storage from cf, create/update personality, add text to image endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { Ai} from '@cloudflare/ai'
 import { Hono } from 'hono'
 
-export interface Env {
+export interface Bindings {
+	MY_BUCKET: R2Bucket
+	MY_NAME: string
 	AI: any
+	MYPEEPS_KV: KVNamespace
 }
 
-const app = new Hono<{ Bindings: Env }>()
+const LLMmodel = '@cf/meta/llama-3-8b-instruct'
+const StableDiffusionModel = '@cf/stabilityai/stable-diffusion-xl-base-1.0'
+
+const app = new Hono<{ Bindings: Bindings }>()
 
 app.get("/chat", async c => {
 	const ai = new Ai(c.env.AI)
@@ -19,8 +25,66 @@ app.get("/chat", async c => {
 
 	const inputs = { messages }
 
-	const response = await ai.run("@cf/mistral/mistral-7b-instruct-v0.1", inputs)
+	const response = await ai.run(LLMmodel, inputs)
 	return c.json(response)
+})
+
+app.get("/picture", async c => {
+	const ai = new Ai(c.env.AI)
+	const inputs = {
+		prompt: c.req.query("prompt")
+	  };
+
+	  const response = await ai.run( StableDiffusionModel, inputs);
+	  return new Response(response, { headers: {'content-type': 'image/png'}});
+})
+
+app.get("/", async c => {
+	const routes = [
+		{name: '/chat', parameter: ['message']},
+	]
+
+	/* write value */
+	await c.env.MYPEEPS_KV.put('name', c.env.MY_NAME)
+	/* read value */
+	const name = await c.env.MYPEEPS_KV.get('name')
+	return c.json(name)
+
+})
+
+// creates/ updates character persistence
+app.post('/character/:id', async c => {
+	const saveInfo = (key, data) => c.env.MYPEEPS_KV.put(key, data);
+	const getInfo = key => c.env.MYPEEPS_KV.get(key);
+	const peepId = c.req.param('id')
+	const characterKey = `${peepId}Character`
+
+	const storedPeep = await getInfo(characterKey)
+	var myPeep
+	if (!storedPeep) {
+		// create myPeep here
+		myPeep = {
+			id: peepId,
+			name: c.req.query('name') || '',
+			contextDescription: c.req.query('contextDescription') || '',
+			personalityDescription: c.req.query('personalityDescription') || '',
+			temperature: c.req.query('temperature') || 0.0
+		}
+	} else {
+		// update myPeep
+		const myPeepLoaded = JSON.parse(storedPeep)
+		myPeep = {ZZ
+			id: peepId,
+			name: c.req.query('name') || myPeepLoaded.name,
+			contextDescription: c.req.query('contextDescription') || myPeepLoaded.contextDescription,
+			personalityDescription: c.req.query('personalityDescription') || myPeepLoaded.personalityDescription,
+			temperature: c.req.query('temperature') || myPeepLoaded.temperature
+		}
+	}
+	// save
+	await saveInfo(characterKey, JSON.stringify(myPeep))
+	return c.json(myPeep)
+
 })
 
 export default app
